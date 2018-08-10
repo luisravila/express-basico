27753e06cbb762c7e2be8868d712dc81f3cd0410
Luis Avila <luis.avila@acid.cl>
2018-08-10 17:37:12 -0400

    Clase

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 const express = require('express');
-
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 
 const app = express();
+
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+//IO = esta la comunicacion del backend
+let io = socketIO(server);
+
+io.on('connection', (client) => {
+    console.log('Usr Connectado');
+
+    client.emit('enviarmensaje', { usuario: 'Admin', mensaje: 'Bienvenido a esta app' });
+
+    client.on('disconnect', () => {
+        console.log('Usuario Desconectado');
+    });
+
+    //escuchar el cliente
+    client.on('enviarmensaje', (msg) => {
+        console.log(msg);
+    });
+});
 
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
